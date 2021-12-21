f8ee13daca3887bdd44a37d3d1f1db25e70afecc
Josh Villanueva <77131654+JoshVL@users.noreply.github.com>
2021-12-20 18:38:07 -0600

    Syntax quick edit

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -7,11 +7,11 @@ const printCards = () => {
   for (const card of CONFIG.cards) {
     
     // Card Item
+    // PREVIOUS: target="${CONFIG.openInNewTab ? '_blank' : ''}"
     let item = `
         <a
           href="${card.link}"
           target="${CONFIG.open ? '_blank' : ''}"
-          // PREVIOUS: target="${CONFIG.openInNewTab ? '_blank' : ''}"
           class="buttons__link  card buttonLink__link-${card.id}"
         >
           <i class="buttonLink__icon" icon-name="${card.icon}"></i>
